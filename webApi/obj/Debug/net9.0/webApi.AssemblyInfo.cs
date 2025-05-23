4e96c09eb1aea42fb5d5ea774dae258bdae5427d
Prasad Chandima <159484298+pcSenavirathna@users.noreply.github.com>
2025-05-23 06:12:56 +0530

    update

--- webApi/obj/Debug/net9.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net9.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6bfcc2b3a7a15158a6c596ebb0ef3b4c27da144")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd38e58639f72d7a1441b2f1f20fb2602d306c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
